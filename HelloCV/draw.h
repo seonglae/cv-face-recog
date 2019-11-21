b246ccfc9a01536e3f086e7e75e6aa43ee6fa2d0
seonglaecho <sungle3737@gmail.com>
2019-11-22 00:06:58 +0900

    😜virtually first commit

---
+++ HelloCV/draw.h
@@ -0,0 +1,7 @@
+#include "opencv2/opencv.hpp"
+
+void drawLines(cv::Mat img);
+void drawPolys(cv::Mat img);
+void drawText(cv::Mat img);
+void centerText(cv::Mat img, cv::String text);
+void changeMat(cv::Mat img);
